14f709e26a23a6957efe3c1439dfe52b595decfd
Rockyph <philipchristianrocki@gmail.com>
2024-06-26 13:07:07 +0200

    af

--- Perceiver_training.py
+++ Perceiver_training.py
@@ -15,7 +15,7 @@ import tqdm
 
 # Hyperparameters
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 160 
+batch_size = 80 
 embedding_size = 32
 latent_size = 16
 attention_heads = 4
